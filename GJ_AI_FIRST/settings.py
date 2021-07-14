0f89711beb0ff47a8748ffa4d228a1203770fad7
KimYeonSang <kek5768@naver.com>
2021-07-14 15:58:13 +0900

    7/14 3 class commit 2

--- GJ_AI_FIRST/settings.py
+++ GJ_AI_FIRST/settings.py
@@ -13,6 +13,9 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.conf.global_settings import LOGIN_REDIRECT_URL
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env_list=dict()
@@ -119,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
@@ -144,3 +147,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL=reverse_lazy('accountapp:hello_world')  #로그인 하면 헬로월드로 이동
+LOGOUT_REDIRECT_URL=reverse_lazy('accountapp:login')            # 로그아웃하면 로그인으로 이동
--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,10 +1,16 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreateView
 
 app_name='accountapp'
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world'),
+    path('hello_world/', hello_world, name='hello_world'),      #html 젤 뒤에 helloworld입력시에 path
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'),
+            name='login'),
+
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create')          #views.py에 create에 접근할 시에,
 ]
