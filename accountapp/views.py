085086deaf1bbe605149ddb3c8239fbdc58881fd
KimYeonSang <kek5768@naver.com>
2021-07-21 16:15:41 +0900

    7/21 3 class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,34 +1,37 @@
 import self as self
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountCreationForm
 from accountapp.models import NEWMODEL
 from django.urls import reverse, reverse_lazy
 
-
+@login_required                     #(login_url=reverse_lazy('accountapp:login'))
 def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method =="POST":
 
-            temp=request.POST.get('input_text')
-            model_instance=NEWMODEL()
-            model_instance.text=temp
-            model_instance.save()
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))      ##class와 function은 불러오는 방식이 달라서 reverse_
 
-        else:
-            data_list=NEWMODEL.objects.all()
-            return render(request, 'accountapp/hello_world.html',
-                          context={'data_list' : data_list})
+
+    if request.method =="POST":
+
+        temp=request.POST.get('input_text')
+        model_instance=NEWMODEL()
+        model_instance.text=temp
+        model_instance.save()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))      ##class와 function은 불러오는 방식이 달라서 reverse_
 
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        data_list=NEWMODEL.objects.all()
+        return render(request, 'accountapp/hello_world.html',context={'data_list' : data_list})
+
+
+
 
 
 class AccountCreateView(CreateView):        #회원가입 해주는 logic
@@ -42,7 +45,8 @@ class AccountDetailView(DetailView):            #내정보 보여주는 로직
     context_object_name = 'target_user'             #target_user룰 통해 연결해준다
     template_name = 'accountapp/detail.html'
 
-
+@method_decorator(login_required,'get')     #AccountUpdateView를 method로 변경login_required를 받아서
+@method_decorator(login_required,'post')
 class AccountUpdateView(UpdateView):
     model=User
     form_class = AccountCreationForm
@@ -50,21 +54,10 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')        #hello world로 돌아가라
     template_name = 'accountapp/update.html'
 
-    def get(self,request,*args,**kwargs):       #login 되어있으면 부모메서드 그대로 상속받기
-        if request.user.is_authenticated and self.get_object() == request.user:
-
-            return super().get(request,*args,**kwargs)
-        else:
-            return HttpResponseForbidden()     #Forbidden  page로 이동
-
-    def post(self,request,*args,**kwargs):
-        if request.user.is_authenticated:
-
-            return super().post(request,*args,**kwargs)
-        else:
-            return HttpResponseForbidden()
 
 
+@method_decorator(login_required,'get')     #AccountUpdateView를 method로 변경login_required를 받아서
+@method_decorator(login_required,'post')
 class AccountDeleteView(DeleteView):
     model=User
     context_object_name = 'target_user'
@@ -72,19 +65,5 @@ class AccountDeleteView(DeleteView):
     template_name = 'accountapp/delete.html'
 
 
-    def get(self,request,*args,**kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
 
-            return super().get(request,*args,**kwargs)
-        else:
-            return HttpResponseForbidden()
-
-    def post(self,request,*args,**kwargs) :
-
-
-        if request.user.is_authenticated and self.get_object() == request.user:
-
-            return super().post(request,*args,**kwargs)
-        else:
-            return HttpResponseForbidden()
 
