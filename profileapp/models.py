dc4a736009c7624b4c1f234b7a613524b961543e
KimYeonSang <kek5768@naver.com>
2021-07-27 10:29:30 +0900

    7/27 3 class commit 2

--- profileapp/models.py
+++ profileapp/models.py
@@ -7,10 +7,10 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,
-                                related_name='profile')
+                                related_name='profile')     #서버에서 직접 할당
 
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=30, unique=True, null=True)
     message = models.CharField(max_length=200, null=True)
-
+#forms.py에서 할당
 # Create your models here.
