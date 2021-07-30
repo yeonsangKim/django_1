b27831caac1071f52f373a3f18d3db3596c81226
KimYeonSang <kek5768@naver.com>
2021-07-30 09:16:42 +0900

    7/30 3 class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -57,8 +57,10 @@ class AccountUpdateView(UpdateView):
     model=User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')        #hello world로 돌아가라
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail',kwargs={'pk':self.object.pk})
 
 
 
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
+from django.urls import reverse
 # Create your views here.
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
@@ -15,12 +15,14 @@ from profileapp.models import Profile
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):     #form안에 이미지 닉네임 메시지가 담겨이씅ㅁ
         form.instance.user=self.request.user    #요청을 보낸 유저가 객체가 된다
         return  super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('accountapp:detail',kwargs={'pk':self.object.user.pk})
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -28,5 +30,8 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     form_class = ProfileCreationForm
     context_object_name = 'target_profile'
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+
+        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
