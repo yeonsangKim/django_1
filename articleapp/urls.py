88b265c0d24ece7dacd38196fbaf7ee093f32159
KimYeonSang <kek5768@naver.com>
2021-08-03 10:43:21 +0900

    8/3 3 class commit 3

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,11 +1,15 @@
+
 from django.urls import path
 from django.views.generic import TemplateView
 
-app_name='articleapp'
+from articleapp.views import ArticleCreateView
+
+app_name = 'articleapp'
 
 urlpatterns = [
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
 
-    path('list/', TemplateView.as_view(template_name='articleapp/list.html'),name='list')
+    path('create/', ArticleCreateView.as_view(), name='create'),
 
 
 ]
