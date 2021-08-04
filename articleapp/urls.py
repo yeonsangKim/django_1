f3f2da5092891812e732d3b33baddce41b1f0332
KimYeonSang <kek5768@naver.com>
2021-08-04 15:45:40 +0900

    8/4 3 class commit 3

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
 
 app_name = 'articleapp'
 
@@ -14,6 +14,8 @@ urlpatterns = [
     path('detail/<int:pk>',ArticleDetailView.as_view(),name='detail'),
 
     path('update/<int:pk>',ArticleUpdateView.as_view(),name='update'),
+
+    path('delete/<int:pk>',ArticleDeleteView.as_view(),name='delete'),
 
 
 ]
