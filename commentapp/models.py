4c2f5f79a03ec29a3402dc05a2fe0e15cc2d1f8e
yeonsangKim <kek5768@naver.com>
2021-08-10 10:27:37 +0900

    8/10 class commit 3

--- commentapp/models.py
+++ commentapp/models.py
@@ -7,7 +7,9 @@ from articleapp.models import Article
 
 class Comment(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
-                               related_name='comment', null=True)
+                               related_name='comment', null=True)  #on_delete=models.SET_NULL는 WIRTER가 탈퇴했을때
+                                                                                            #게시글은 유지되면서 WRITER가 NULL로 바뀜
+                                                                                            #CASCADE는 게시글 자체가 없어짐
     article = models.ForeignKey(Article, on_delete=models.SET_NULL,
                                 related_name='comment', null=True)
 
