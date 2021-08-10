e3ef931cd65d3b1b2706b30c77bd3a31675ee4ec
yeonsangKim <kek5768@naver.com>
2021-08-10 09:30:17 +0900

    8/10 class commit 2

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articleapp.models import Article
+
+
+class Comment(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL,
+                                                related_name='comment', null=True)
+
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL,
+                                                    related_name='comment', null=True)
+
+    comment = models.TextField(null=False)        #장문의 글을 받을때, 짧은글은 charField
+
+    created_at = models.DateTimeField(auto_now_add=True)
