c39e982254477332c93c7c98ab6bbf3c72add7bd
yeonsangKim <kek5768@naver.com>
2021-08-11 14:58:38 +0900

    8/11 class commit 2

---
+++ commentapp/decorator.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponseForbidden
+
+from commentapp.models import Comment
+
+
+def comment_ownership_required(func) :
+    def decorated(request,*args,**kwargs) :
+        target_comment = Comment.objects.get(pk=kwargs['pk']) # db에서 뽑아옴
+        if target_comment.writer == request.user :
+            return func(request, *args, **kwargs)
+        else :
+            return  HttpResponseForbidden()
+
+    return  decorated()
